91ba3cec3ae82aa23b3ab4056a8cf0dc288a7a4a
shimon <shimon002@gmail.com>
2026-03-09 23:49:34 +0200

    Allow all hosts for Vite server and preview (Arxentra deployment)

--- my_new_project/vite.config.ts
+++ my_new_project/vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: { allowedHosts: 'all' },
+	preview: { allowedHosts: 'all' },
 	optimizeDeps: {
 		exclude: ['better-sqlite3'],
 	},
